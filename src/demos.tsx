0a760921b17d03345101a3ab48cb5d18522377b7
james <jnchin.james@gmail.com>
2025-01-08 16:01:21 -0500

    logo

--- src/demos.tsx
+++ src/demos.tsx
@@ -1,12 +1,21 @@
+import imgUrl from "./img/react-logo.png";
+import './App.css'
+
 function Demos() {
   /*const element = createElement("div", {
     className: "container",
     children: ["Hello World", "Goodbye World"],
   }
     );*/
-  const first = 'Joe';
-  const element = <div className="container">Hello {first}</div>;
+  const logo = {
+    name: "React Logo",
+    title: "React Logo",
+    path: imgUrl,
+  };
 
+  const element = (
+    <img className="logo" src={logo.path} alt={logo.name} title={logo.title}/>
+  );
 
   console.log(element);
   return element;
